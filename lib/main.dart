503e8ffc07a230174a1fe4d9f28ca2780e35a6b1
Mark Faust <markfaust123@gmail.com>
2023-07-12 09:43:41 -0400

    Completed app! Has the magic 8 ball functionality, using a Gesture Detector.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Ball(),
+      ),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('I got clicked: $ballNumber');
+        setState(() {
+          ballNumber = Random().nextInt(5) + 1;
+        });
+      },
+      child: Image.asset('images/ball$ballNumber.png'),
+    );
+  }
+}
